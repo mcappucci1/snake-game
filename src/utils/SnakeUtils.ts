5ee7c79c7c83b5afc79212431cefd903e1f7c8d4
mcappucci34 <m.cappucci34@gmail.com>
2022-08-27 19:14:40 -0400

    Change starting position

--- src/utils/SnakeUtils.ts
+++ src/utils/SnakeUtils.ts
@@ -22,11 +22,11 @@ export type SnakeCell = {
 }
 
 export const SNAKE_START_POSITION: SnakeCell[] = [
-    { x: 12, y: 10 },
-    { x: 11, y: 10 },
-    { x: 10, y: 10 },
-    { x: 9, y: 10 },
-    { x: 8, y: 10}
+    { x: 6, y: 10 },
+    { x: 5, y: 10 },
+    { x: 4, y: 10 },
+    { x: 3, y: 10 },
+    { x: 2, y: 10}
 ];
 export const NUM_CELLS = 21;
 
